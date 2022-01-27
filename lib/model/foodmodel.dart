8e6127db14f0f2e61cb4efc4b54bd598a7283080
zihad650 <zihad650top@gmail.com>
2022-01-27 13:44:23 +0600

    food json

--- lib/model/foodmodel.dart
+++ lib/model/foodmodel.dart
@@ -2,9 +2,10 @@ class FoodModel{
   String? id;
   String? name;
   String? title;
+  String? subtitle;
   String? category;
   String? description;
   String? image;
 
-  FoodModel(this.id, this.name,this.title, this.category, this.description, this.image);
+  FoodModel(this.id, this.name,this.title, this.subtitle, this.category, this.description, this.image);
 }
